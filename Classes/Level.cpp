54179de86bc0074b1efbc6693ac53198073ea541
srpatel <sunil@xikka.com>
2017-01-11 22:27:38 +0000

    Different level structure

--- Classes/Level.cpp
+++ Classes/Level.cpp
@@ -26,27 +26,33 @@ Round *LevelManager::generateRound(int stage) {
 	} else if (stage == 1) {
 		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 	} else if (stage == 2) {
-		r->monsters.push_back(MonsterManager::get()->get("goblin_sword"));
+		r->monsters.push_back(MonsterManager::get()->get("dog"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_sword"));
 	} else if (stage == 3) {
-		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_sword"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 	} else if (stage == 4) {
+		r->monsters.push_back(MonsterManager::get()->get("dog"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
-		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 	} else if (stage == 5) {
-		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
-		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 	} else if (stage == 6) {
-		r->monsters.push_back(MonsterManager::get()->get("goblin_sword"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 	} else if (stage == 7) {
+		r->monsters.push_back(MonsterManager::get()->get("dog"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
+	} else if (stage == 8) {
+		r->monsters.push_back(MonsterManager::get()->get("goblin_sword"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
+		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
+	} else if (stage == 9) {
 		r->monsters.push_back(MonsterManager::get()->get("goblin_axe"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 	} else {
-		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
+		r->monsters.push_back(MonsterManager::get()->get((rand() % 2 == 0) ? "goblin_halberd" : "dog"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 		r->monsters.push_back(MonsterManager::get()->get("goblin_halberd"));
 	}
