89958816c43a86f92581cb055e5b1f9cebe843fc
srpatel <sunil@xikka.com>
2017-08-29 07:41:33 +0100

    Add start arena button.

--- Classes/ArenaScreen.cpp
+++ Classes/ArenaScreen.cpp
@@ -21,8 +21,22 @@ bool ArenaScreen::init() {
 		return false;
 	}
 	
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	setContentSize(visibleSize);
+	Size size = Director::getInstance()->getVisibleSize();
+	setContentSize(size);
+	
+	auto button = ui::Button::create("ui/button_up.png", "ui/button_down.png", "ui/button_down.png", TEXTURE_TYPE);
+	button->setTitleFontName(Fonts::TEXT_FONT);
+	button->setTitleFontSize(Fonts::TEXT_SIZE);
+	button->setPosition(Vec2(size.width/2, 50 + button->getContentSize().height/2));
+	button->setTitleText _("ui.START_ARENA");
+	button->addTouchEventListener([button](Ref* pSender, ui::Widget::TouchEventType type) {
+		if (type == ui::Widget::TouchEventType::ENDED) {
+			button->setTouchEnabled(false);
+			PLAY_SOUND( kSoundEffect_UISelect );
+			GameController::get()->startArena();
+		}
+	});
+	this->addChild(button);
 	
 	return true;
 }
